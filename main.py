5bdd451b9f5343aed10a5cd3bf2dc263c288a3ea
LouisCleriot <louis.cleriot@telecomnancy.net>
2026-02-15 12:55:55 -0500

    text book version of self attention, no opti

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import torch
+from src.layers.attention.selfAttention import SelfAttentionTextBook
+
 def main():
-    print("Hello from neuro-forge!")
+    embedingSize = 1024
+    attentionSize = 128
+    selfAttention = SelfAttentionTextBook(embedingSize, attentionSize, "xavier")
+    token_len = 5
+    x = torch.randn(1, token_len, embedingSize)
+    mask = torch.tril(torch.ones(token_len, token_len))
+    out = selfAttention(x, mask)
 
 
 if __name__ == "__main__":
---
+++ src/layers/attention/selfAttention.py
@@ -0,0 +1,49 @@
+from typing import Literal
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import logging
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+class SelfAttentionTextBook(nn.Module):
+    def __init__(self, embeding_size: int, attention_dim: int, initialisation: Literal["xavier", "glorot", "he"]="xavier") -> None :
+        super().__init__()
+        
+        self.dk = attention_dim
+        self.WQ = nn.Linear(in_features=embeding_size, out_features=attention_dim, bias=False)
+        self.WK = nn.Linear(in_features=embeding_size, out_features=attention_dim, bias=False)
+        self.WV = nn.Linear(in_features=embeding_size, out_features=embeding_size, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+
+        if initialisation=="xavier":
+            nn.init.xavier_normal_(self.WQ.weight)
+            nn.init.xavier_normal_(self.WK.weight)
+            nn.init.xavier_normal_(self.WV.weight)
+        elif initialisation=="glorot":
+            nn.init.xavier_uniform_(self.WQ.weight)
+            nn.init.xavier_uniform_(self.WK.weight)
+            nn.init.xavier_uniform_(self.WV.weight)
+        elif initialisation=="he":
+            nn.init.kaiming_normal_(self.WQ.weight)
+            nn.init.kaiming_normal_(self.WK.weight)
+            nn.init.kaiming_normal_(self.WV.weight)
+        else:
+            logger.error(f"Initialisation {initialisation} not supported. Please choose between 'xavier','glorot' and 'random'.")
+            raise ValueError("Initialisation not supported. Please choose between 'xavier','glorot' and 'random'.")
+
+    def forward(self, x: torch.tensor, mask: torch.tensor = None):
+        # x shape: (Batch, Seq_Len, Embedding_Size)
+
+        q = self.WQ(x)   
+        k = self.WK(x)
+        v = self.WV(x)
+        
+        scores = torch.matmul(q,k.transpose(-2, -1)) / np.sqrt(self.dk)
+        
+        if mask is not None: 
+            scores = scores.masked_fill(mask==0, float("-inf"))
+        attention_weight = F.softmax(scores, dim=-1)
+        output = torch.matmul(attention_weight, v)
+        return output
