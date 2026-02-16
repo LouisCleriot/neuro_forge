554cf84488ebe4757d7afb3bf5b887ca9bf8b349
Coralie serrand <coralie.serrand@gmail.com>
2026-02-15 20:38:36 -0500

    add kv cache

--- src/layers/attention/multiHeadAttention.py
+++ src/layers/attention/multiHeadAttention.py
@@ -12,6 +12,8 @@ class MultiHeadAttention(nn.Module):
         self.n_heads = n_heads
         self.w_qkv_h = nn.Linear(in_features=embedding_size,out_features=3*self.dk*n_heads, bias=False)
         self.wo = nn.Linear(in_features=embedding_size, out_features=embedding_size, bias=False)
+        self.k_cache = None
+        self.v_cache = None
 
         if initialisation=="xavier":
             nn.init.xavier_normal_(self.w_qkv_h.weight)
@@ -24,18 +26,21 @@ class MultiHeadAttention(nn.Module):
             raise ValueError("Initialisation not supported. Please choose between 'xavier','glorot' and 'random'.")
 
 
-    def forward(self, x: torch.tensor, mask: torch.tensor = None):
+    def forward(self, x: torch.tensor, mask: torch.tensor = None, use_cache: bool = True):
         # (batch, len, emb size)
         qkv = self.w_qkv_h(x)
-        
         q,k,v = rearrange(qkv, "b l (three h d) -> three b h l d", three=3, h=self.n_heads)
-
+        
+        if use_cache :
+            if self.k_cache is not None :
+                k = torch.cat((self.k_cache, k), dim=-2)
+                v = torch.cat((self.v_cache, v), dim=-2)
+            self.k_cache=k.detach()
+            self.v_cache=v.detach()
+          
         scores = torch.matmul(q, k.transpose(-2, -1)) / np.sqrt(self.dk)
-
         if mask is not None:
             scores = scores.masked_fill(mask==0, float("-inf"))
-        output = F.softmax(scores) @ v
-        
+        output = F.softmax(scores, dim=-1) @ v
         output = rearrange(output, "b h l d -> b l (h d)", h=self.n_heads)
-        
         return self.wo(output)
